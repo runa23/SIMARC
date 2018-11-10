12f9baf1a241682aab70cd4e27a2d464b8db0138
runa23 <septhiandri@gmail.com>
2018-11-10 19:06:57 +0700

    Update Frm_Outlet
    Update Form Outlet terbaru

--- SIMARC/FrmLokasi.Designer.vb
+++ SIMARC/FrmLokasi.Designer.vb
@@ -22,8 +22,8 @@ Partial Class FrmLokasi
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmLokasi))
-        Me.DgLokasi = New System.Windows.Forms.DataGridView()
         Me.BtnKeluar = New System.Windows.Forms.Button()
         Me.BtnBatal = New System.Windows.Forms.Button()
         Me.BtnHapus = New System.Windows.Forms.Button()
@@ -37,26 +37,28 @@ Partial Class FrmLokasi
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.TxtKode = New System.Windows.Forms.TextBox()
+        Me.SIMARCData_Set = New SIMARC.SIMARCData_Set()
+        Me.LokasiBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LokasiTableAdapter = New SIMARC.SIMARCData_SetTableAdapters.LokasiTableAdapter()
+        Me.TableAdapterManager = New SIMARC.SIMARCData_SetTableAdapters.TableAdapterManager()
+        Me.DgLokasi = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.SIMARCData_Set, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LokasiBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DgLokasi, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'DgLokasi
-        '
-        Me.DgLokasi.AllowUserToAddRows = False
-        Me.DgLokasi.AllowUserToDeleteRows = False
-        Me.DgLokasi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DgLokasi.Location = New System.Drawing.Point(12, 279)
-        Me.DgLokasi.Name = "DgLokasi"
-        Me.DgLokasi.ReadOnly = True
-        Me.DgLokasi.Size = New System.Drawing.Size(458, 203)
-        Me.DgLokasi.TabIndex = 47
         '
         'BtnKeluar
         '
         Me.BtnKeluar.BackgroundImage = Global.SIMARC.My.Resources.Resources.door_out
         Me.BtnKeluar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.BtnKeluar.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnKeluar.Location = New System.Drawing.Point(364, 224)
+        Me.BtnKeluar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnKeluar.Location = New System.Drawing.Point(331, 224)
         Me.BtnKeluar.Name = "BtnKeluar"
         Me.BtnKeluar.Size = New System.Drawing.Size(71, 49)
         Me.BtnKeluar.TabIndex = 46
@@ -68,8 +70,8 @@ Partial Class FrmLokasi
         '
         Me.BtnBatal.BackgroundImage = Global.SIMARC.My.Resources.Resources.Cancel_16x16
         Me.BtnBatal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.BtnBatal.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnBatal.Location = New System.Drawing.Point(287, 224)
+        Me.BtnBatal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBatal.Location = New System.Drawing.Point(254, 224)
         Me.BtnBatal.Name = "BtnBatal"
         Me.BtnBatal.Size = New System.Drawing.Size(71, 49)
         Me.BtnBatal.TabIndex = 45
@@ -81,8 +83,8 @@ Partial Class FrmLokasi
         '
         Me.BtnHapus.BackgroundImage = Global.SIMARC.My.Resources.Resources.Delete_16x16
         Me.BtnHapus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.BtnHapus.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnHapus.Location = New System.Drawing.Point(210, 224)
+        Me.BtnHapus.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnHapus.Location = New System.Drawing.Point(177, 224)
         Me.BtnHapus.Name = "BtnHapus"
         Me.BtnHapus.Size = New System.Drawing.Size(71, 49)
         Me.BtnHapus.TabIndex = 44
@@ -94,8 +96,8 @@ Partial Class FrmLokasi
         '
         Me.BtnEdit.BackgroundImage = Global.SIMARC.My.Resources.Resources.Edit_16x16
         Me.BtnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.BtnEdit.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnEdit.Location = New System.Drawing.Point(133, 224)
+        Me.BtnEdit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEdit.Location = New System.Drawing.Point(100, 224)
         Me.BtnEdit.Name = "BtnEdit"
         Me.BtnEdit.Size = New System.Drawing.Size(71, 49)
         Me.BtnEdit.TabIndex = 43
@@ -107,8 +109,8 @@ Partial Class FrmLokasi
         '
         Me.BtnSimpan.BackgroundImage = Global.SIMARC.My.Resources.Resources.Save_16x16
         Me.BtnSimpan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.BtnSimpan.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BtnSimpan.Location = New System.Drawing.Point(56, 224)
+        Me.BtnSimpan.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSimpan.Location = New System.Drawing.Point(23, 224)
         Me.BtnSimpan.Name = "BtnSimpan"
         Me.BtnSimpan.Size = New System.Drawing.Size(71, 49)
         Me.BtnSimpan.TabIndex = 42
@@ -118,7 +120,7 @@ Partial Class FrmLokasi
         '
         'TxtTelp
         '
-        Me.TxtTelp.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtTelp.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtTelp.Location = New System.Drawing.Point(140, 194)
         Me.TxtTelp.Multiline = True
         Me.TxtTelp.Name = "TxtTelp"
@@ -127,16 +129,16 @@ Partial Class FrmLokasi
         '
         'TxtAlamat
         '
-        Me.TxtAlamat.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtAlamat.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtAlamat.Location = New System.Drawing.Point(140, 72)
         Me.TxtAlamat.Multiline = True
         Me.TxtAlamat.Name = "TxtAlamat"
-        Me.TxtAlamat.Size = New System.Drawing.Size(321, 116)
+        Me.TxtAlamat.Size = New System.Drawing.Size(262, 116)
         Me.TxtAlamat.TabIndex = 40
         '
         'TxtNama
         '
-        Me.TxtNama.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtNama.Location = New System.Drawing.Point(140, 42)
         Me.TxtNama.Multiline = True
         Me.TxtNama.Name = "TxtNama"
@@ -145,7 +147,7 @@ Partial Class FrmLokasi
         '
         'Label1
         '
-        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(12, 193)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(111, 25)
@@ -155,7 +157,7 @@ Partial Class FrmLokasi
         '
         'Label3
         '
-        Me.Label3.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.Location = New System.Drawing.Point(12, 71)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(111, 25)
@@ -165,7 +167,7 @@ Partial Class FrmLokasi
         '
         'Label2
         '
-        Me.Label2.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.Location = New System.Drawing.Point(12, 42)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(111, 25)
@@ -175,7 +177,7 @@ Partial Class FrmLokasi
         '
         'Label4
         '
-        Me.Label4.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.Location = New System.Drawing.Point(12, 12)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(111, 25)
@@ -185,18 +187,101 @@ Partial Class FrmLokasi
         '
         'TxtKode
         '
-        Me.TxtKode.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtKode.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtKode.Location = New System.Drawing.Point(140, 12)
         Me.TxtKode.Multiline = True
         Me.TxtKode.Name = "TxtKode"
         Me.TxtKode.Size = New System.Drawing.Size(222, 24)
         Me.TxtKode.TabIndex = 39
         '
+        'SIMARCData_Set
+        '
+        Me.SIMARCData_Set.DataSetName = "SIMARCData_Set"
+        Me.SIMARCData_Set.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'LokasiBindingSource
+        '
+        Me.LokasiBindingSource.DataMember = "Lokasi"
+        Me.LokasiBindingSource.DataSource = Me.SIMARCData_Set
+        '
+        'LokasiTableAdapter
+        '
+        Me.LokasiTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.Connection = Nothing
+        Me.TableAdapterManager.KaryawanTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = SIMARC.SIMARCData_SetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'DgLokasi
+        '
+        Me.DgLokasi.AllowUserToAddRows = False
+        Me.DgLokasi.AllowUserToDeleteRows = False
+        Me.DgLokasi.AllowUserToOrderColumns = True
+        Me.DgLokasi.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DgLokasi.AutoGenerateColumns = False
+        Me.DgLokasi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgLokasi.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn6, Me.DataGridViewTextBoxColumn5})
+        Me.DgLokasi.DataSource = Me.LokasiBindingSource
+        Me.DgLokasi.Location = New System.Drawing.Point(12, 279)
+        Me.DgLokasi.Name = "DgLokasi"
+        Me.DgLokasi.ReadOnly = True
+        Me.DgLokasi.Size = New System.Drawing.Size(398, 277)
+        Me.DgLokasi.TabIndex = 47
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "kd_lokasi"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Kode"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "nm_lokasi"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Nama Outlet"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "almt_lokasi"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Lokasi Outlet"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.ReadOnly = True
+        Me.DataGridViewTextBoxColumn3.Width = 200
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "tlp_lokasi"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Telp"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn6
+        '
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "nm_karyawan"
+        Me.DataGridViewTextBoxColumn6.HeaderText = "Created By"
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
+        Me.DataGridViewTextBoxColumn6.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "kd_karyawan"
+        Me.DataGridViewTextBoxColumn5.HeaderText = "kd_karyawan"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        Me.DataGridViewTextBoxColumn5.ReadOnly = True
+        Me.DataGridViewTextBoxColumn5.Visible = False
+        '
         'FrmLokasi
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(483, 526)
+        Me.ClientSize = New System.Drawing.Size(422, 568)
         Me.Controls.Add(Me.DgLokasi)
         Me.Controls.Add(Me.BtnKeluar)
         Me.Controls.Add(Me.BtnBatal)
@@ -215,12 +300,13 @@ Partial Class FrmLokasi
         Me.Name = "FrmLokasi"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Outlet"
+        CType(Me.SIMARCData_Set, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LokasiBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DgLokasi, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents DgLokasi As System.Windows.Forms.DataGridView
     Friend WithEvents BtnKeluar As System.Windows.Forms.Button
     Friend WithEvents BtnBatal As System.Windows.Forms.Button
     Friend WithEvents BtnHapus As System.Windows.Forms.Button
@@ -234,4 +320,15 @@ Partial Class FrmLokasi
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents TxtKode As System.Windows.Forms.TextBox
+    Friend WithEvents SIMARCData_Set As SIMARC.SIMARCData_Set
+    Friend WithEvents LokasiBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents LokasiTableAdapter As SIMARC.SIMARCData_SetTableAdapters.LokasiTableAdapter
+    Friend WithEvents TableAdapterManager As SIMARC.SIMARCData_SetTableAdapters.TableAdapterManager
+    Friend WithEvents DgLokasi As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As System.Windows.Forms.DataGridViewTextBoxColumn
 End Class
--- SIMARC/FrmLokasi.vb
+++ SIMARC/FrmLokasi.vb
@@ -1,30 +1,12 @@
 ﻿Imports System.Data
 Imports System.Data.SqlClient
+Imports SIMARC.Modul
 Public Class FrmLokasi
     Dim kode As String
 
     Private Sub FrmLokasi_Load(sender As Object, e As EventArgs) Handles Me.Load
-        TampilData()
-        SetTombol1()
-    End Sub
-    Sub TampilData()
-        OpenConnection()
-        sql = "Select * from Lokasi"
-        da = New SqlDataAdapter(sql, con)
-        ds = New DataSet
-        da.Fill(ds, 0)
-        DgLokasi.DataSource = ds.Tables(0)
-        SetHeader()
-        CloseConnection()
-    End Sub
-    Sub SetHeader()
-        With DgLokasi
-            .Columns(0).HeaderText = "Kode"
-            .Columns(1).HeaderText = "Nama"
-            .Columns(2).HeaderText = "Alamat"
-            .Columns(3).HeaderText = "Telp"
-            .Columns(4).HeaderText = "Karyawan"
-        End With
+        LokasiTableAdapter.Fill(Me.SIMARCData_Set.Lokasi)
+        SetTombol(True)
     End Sub
     Sub Clear()
         TxtNama.Clear()
@@ -32,19 +14,15 @@ Public Class FrmLokasi
         TxtTelp.Clear()
         TxtKode.Clear()
     End Sub
-    Sub SetTombol1()
-        BtnSimpan.Enabled = True
-        BtnEdit.Enabled = False
-        BtnHapus.Enabled = False
-        BtnBatal.Enabled = False
-        BtnKeluar.Enabled = True
+    Sub ReloadData()
+        LokasiTableAdapter.Fill(SIMARCData_Set.Lokasi)
     End Sub
-    Sub SetTombol2()
-        BtnSimpan.Enabled = False
-        BtnEdit.Enabled = True
-        BtnHapus.Enabled = True
-        BtnBatal.Enabled = True
-        BtnKeluar.Enabled = False
+    Sub SetTombol(ByVal st As Boolean)
+        BtnSimpan.Enabled = st
+        BtnEdit.Enabled = Not st
+        BtnHapus.Enabled = Not st
+        BtnBatal.Enabled = Not st
+        BtnKeluar.Enabled = st
     End Sub
     Sub Insert()
         If TxtKode.Text = "" Or TxtNama.Text = "" Or TxtAlamat.Text = "" Or TxtTelp.Text = "" Then
@@ -57,12 +35,12 @@ Public Class FrmLokasi
                   "','" & TxtTelp.Text & "','" & Menu_Utama.Kd_Karyawan.Text & "'"
                 cmd = New SqlCommand(sql, con)
                 cmd.ExecuteNonQuery()
-                MessageBox.Show("Data Berhasil di Simpan", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                MessageBox.Show("Data Berhasil di Simpan", "Informasi", MessageBoxButtons.OK)
             Catch ex As Exception
                 MsgBox(ex.Message)
             End Try
             Clear()
-            TampilData()
+            ReloadData()
             CloseConnection()
         End If
     End Sub
@@ -77,13 +55,13 @@ Public Class FrmLokasi
                   "','" & Menu_Utama.Kd_Karyawan.Text & "'"
                 cmd = New SqlCommand(sql, con)
                 cmd.ExecuteNonQuery()
-                MessageBox.Show("Data Berhasil di Edit", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                MessageBox.Show("Data Berhasil di Edit", "Informasi", MessageBoxButtons.OK)
             Catch ex As Exception
                 MsgBox(ex.Message)
             End Try
-            SetTombol1()
+            SetTombol(True)
             Clear()
-            TampilData()
+            ReloadData()
             CloseConnection()
         End If
     End Sub
@@ -98,19 +76,11 @@ Public Class FrmLokasi
             MessageBox.Show("Data sedang digunakan", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End Try
         Clear()
-        TampilData()
-        SetTombol1()
+        ReloadData()
+        SetTombol(True)
         CloseConnection()
     End Sub
-    Private Sub DgLokasi_DoubleClick(sender As Object, e As EventArgs) Handles DgLokasi.DoubleClick
-        With DgLokasi
-            TxtKode.Text = .Item(0, .CurrentRow.Index).Value
-            TxtNama.Text = .Item(1, .CurrentRow.Index).Value
-            TxtAlamat.Text = .Item(2, .CurrentRow.Index).Value
-            TxtTelp.Text = .Item(3, .CurrentRow.Index).Value
-        End With
-        SetTombol2()
-    End Sub
+   
 
     Private Sub BtnSimpan_Click(sender As Object, e As EventArgs) Handles BtnSimpan.Click
         Insert()
@@ -126,10 +96,24 @@ Public Class FrmLokasi
 
     Private Sub BtnBatal_Click(sender As Object, e As EventArgs) Handles BtnBatal.Click
         Clear()
-        SetTombol1()
+        SetTombol(True)
     End Sub
 
     Private Sub BtnKeluar_Click(sender As Object, e As EventArgs) Handles BtnKeluar.Click
         Dispose()
+    End Sub
+
+    Private Sub DgLokasi_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgLokasi.CellContentClick
+        With DgLokasi.Rows(e.RowIndex)
+            TxtKode.Text = .Cells(0).Value
+            TxtNama.Text = .Cells(1).Value
+            TxtAlamat.Text = .Cells(2).Value
+            TxtTelp.Text = .Cells(3).Value
+        End With
+        SetTombol(False)
+    End Sub
+
+    Private Sub TxtTelp_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtTelp.KeyPress
+        HanyaAngka(e)
     End Sub
 End Class
